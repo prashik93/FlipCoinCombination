4337a46d8133054066717259d153e442c22e6685
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-13 13:16:06 +0530

    [Prashik]Refactor the code to add doublet combinations and percentages

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -8,36 +8,82 @@ isTail=0
 headCount=0
 tailCount=0
 
-count=0
-whileIteration=20
+countHH=0
+countHT=0
+countTH=0
+countTT=0
 
-declare -A resultDictionary
+whileIteration=50
+
+
+declare -A singletDictionary
+declare -A doubletDictionary
 
 while ((count < $whileIteration))
 do
-	flipCoin=$((RANDOM%2))
-
-	if [ $flipCoin -eq $isHead ]
+	flipCoin1=$((RANDOM%2))
+	flipCoin2=$((RANDOM%2))
+	if [ $flipCoin1 -eq $isHead ]
 	then
 		headCount=$((headCount + 1))
-		resultDictionary["Head"]=$headCount
+		percentageOfHeadCount=$(( (($headCount*100)/$whileIteration) ))
+		singletDictionary["Head"]=$headCount
 
-	elif [ $flipCoin -eq $isTail ]
+	elif [ $flipCoin1 -eq $isTail ]
 	then
 		tailCount=$((tailCount + 1))
-		resultDictionary["Tail"]=$tailCount
+		percentageOfTailCount=$(( (($tailCount*100)/$whileIteration) ))
+		singletDictionary["Tail"]=$tailCount
+	fi
+
+
+	if [[ ($flipCoin1 -eq $isHead) && ($flipCoin2 -eq $isHead) ]]
+	then
+		countHH=$((countHH + 1))
+		countPercentage_HH=$(( (($countHH*100)/$whileIteration) ))
+		doubletDictionary["HH"]=$countHH
+		doubletDictionary["HH_per"]=$countPercentage_HH
+
+	elif [[ ($flipCoin1 -eq $isHead) && ($flipCoin2 -eq $isTail) ]]
+	then
+		countHT=$((countHT +1))
+		countPercentage_HT=$(( (($countHT*100)/$whileIteration) ))
+		doubletDictionary["HT"]=$countHT
+		doubletDictionary["HT_per"]=$countPercentage_HT
+
+	elif [[ ($flipCoin1 -eq $isTail) && ($flipCoin2 -eq $isHead) ]]
+	then
+		countTH=$((countTH +1))
+		countPercentage_TH=$(( (($countTH*100)/$whileIteration) ))
+		doubletDictionary["TH"]=$countTH
+		doubletDictionary["TH_per"]=$countPercentage_TH
+
+	elif [[ ($flipCoin1 -eq $isTail) && ($flipCoin2 -eq $isTail) ]]
+	then
+		countTT=$((countTT +1))
+		countPercentage_TT=$(( (($countTT*100)/$whileIteration) ))
+		doubletDictionary["TT"]=$countTT
+		doubletDictionary["TT_per"]=$countPercentage_TT
 
 	fi
 	((count++))
 done
 
-echo "Result dictionary Head Count :- ${resultDictionary["Head"]}"
-echo "Result dictionary Tail Count :- ${resultDictionary["Tail"]}"
-echo "Result dictionary Keys :- ${!resultDictionary[@]}"
-echo "Result dictionary Values :- ${resultDictionary[@]}"
+echo "--------Singlet Combination-------"
 
-percentageOfHeadCount=$(( (($headCount*100)/$whileIteration) ))
+echo "Singlet dictionary Head Count :- ${singletDictionary["Head"]}"
+echo "Singlet dictionary Tail Count :- ${singletDictionary["Tail"]}"
+echo "Singlet dictionary Keys :- ${!singletDictionary[@]}"
+echo "Singlet dictionary Values :- ${singletDictionary[@]}"
 echo "Percentage Head count is :- $percentageOfHeadCount %"
-
-percentageOfTailCount=$(( (($tailCount*100)/$whileIteration) ))
 echo "Percentage Tail count is :- $percentageOfTailCount %"
+
+
+echo "--------Doublet Combination--------"
+
+echo "Doublet dictionary HH Count :- ${doubletDictionary["HH"]}"
+echo "Doublet dictionary HT Count :- ${doubletDictionary["HT"]}"
+echo "Doublet dictionary TH Count :- ${doubletDictionary["TH"]}"
+echo "Doublet dictionary TT Count :- ${doubletDictionary["TT"]}"
+echo "Doublet dictionary Keys :- ${!doubletDictionary[@]}"
+echo "Doublet dictionary Values :- ${doubletDictionary[@]}"
