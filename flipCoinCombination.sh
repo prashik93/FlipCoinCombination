c2fb4f159f69419eaaa9440f8c58787bae7d354b
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-13 11:53:02 +0530

    [Prashik]Add code to store the result in dictionary and calculate the percentage of singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,17 +2,42 @@
 
 echo "Welcome to Flip coin combination"
 
-flipCoin=$((RANDOM%2))
-
 isHead=1
 isTail=0
 
-if [ $flipCoin -eq $isHead ]
-then
-	echo "Heads"
+headCount=0
+tailCount=0
 
-elif [ $flipCoin -eq $isTail ]
-then
-	echo "Tails"
+count=0
+whileIteration=20
 
-fi
+declare -A resultDictionary
+
+while ((count < $whileIteration))
+do
+	flipCoin=$((RANDOM%2))
+
+	if [ $flipCoin -eq $isHead ]
+	then
+		headCount=$((headCount + 1))
+		resultDictionary["Head"]=$headCount
+
+	elif [ $flipCoin -eq $isTail ]
+	then
+		tailCount=$((tailCount + 1))
+		resultDictionary["Tail"]=$tailCount
+
+	fi
+	((count++))
+done
+
+echo "Result dictionary Head Count :- ${resultDictionary["Head"]}"
+echo "Result dictionary Tail Count :- ${resultDictionary["Tail"]}"
+echo "Result dictionary Keys :- ${!resultDictionary[@]}"
+echo "Result dictionary Values :- ${resultDictionary[@]}"
+
+percentageOfHeadCount=$(( (($headCount*100)/$whileIteration) ))
+echo "Percentage Head count is :- $percentageOfHeadCount %"
+
+percentageOfTailCount=$(( (($tailCount*100)/$whileIteration) ))
+echo "Percentage Tail count is :- $percentageOfTailCount %"
