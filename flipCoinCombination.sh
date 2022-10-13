051954fe439e36e33d344cee099c914ffea87668
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-13 10:41:50 +0530

    [Prashik]Add code for flipcoin to check head or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,18 @@
+#! /bin/bash/ -x
+
 echo "Welcome to Flip coin combination"
+
+flipCoin=$((RANDOM%2))
+
+isHead=1
+isTail=0
+
+if [ $flipCoin -eq $isHead ]
+then
+	echo "Heads"
+
+elif [ $flipCoin -eq $isTail ]
+then
+	echo "Tails"
+
+fi
